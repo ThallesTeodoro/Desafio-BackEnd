587cba6dd4ec03a75385fb84b1565b27f5cde4d4
Thalles Teodoro <thallesmjteodoro@outlook.com>
2024-05-21 07:51:54 -0300

    feat: add login tests

---
+++ tests/DesafioBackEnd.UnitTests/Application/User/Login/LoginCommandHandlerTests.cs
@@ -0,0 +1,76 @@
+using DesafioBackEnd.Application.Exceptions;
+using DesafioBackEnd.Application.User.Login;
+using DesafioBackEnd.Domain.Contracts.Authentication;
+using DesafioBackEnd.Domain.Contracts.Persistence;
+using FluentAssertions;
+using Moq;
+
+namespace DesafioBackEnd.UnitTests.Application.User.Login;
+
+public class LoginCommandHandlerTests
+{
+    private readonly Mock<IUserRepository> _userRepositoryMock;
+    private readonly Mock<IJwtProvider> _jwtProviderMock;
+
+    public LoginCommandHandlerTests()
+    {
+        _userRepositoryMock = new();
+        _jwtProviderMock = new();
+    }
+
+    [Fact]
+    public void Handle_Should_ReturnUnauthorizedException_WhenUserWasNotFound()
+    {
+        var command = new LoginCommand("usernotfound@email.com");
+
+        _userRepositoryMock
+            .Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
+            .ReturnsAsync((Domain.Entities.User?)null);
+
+        var handler = new LoginCommandHandler(_userRepositoryMock.Object, _jwtProviderMock.Object);
+
+        Func<Task> func = async () => await handler.Handle(command, default);
+
+        func.Should().ThrowAsync<UnauthorizedException>();
+    }
+
+    [Fact]
+    public void Handle_Should_ReturnTheToken_WhenUserWasFound()
+    {
+        var email = "usernotfound@email.com";
+        var command = new LoginCommand(email);
+        var user = new Domain.Entities.User()
+        {
+            Id = Guid.NewGuid(),
+            Email = email,
+            Name = "User Test"
+        };
+
+        _userRepositoryMock
+            .Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
+            .ReturnsAsync(new Domain.Entities.User()
+            {
+                Id = Guid.NewGuid(),
+                Email = email,
+                Name = "User Test"
+            });
+
+        _jwtProviderMock
+            .Setup(x => x.Generate(user))
+            .Returns(RandomToken(273));
+
+        var handler = new LoginCommandHandler(_userRepositoryMock.Object, _jwtProviderMock.Object);
+
+        Func<Task> func = async () => await handler.Handle(command, default);
+
+        func.Should().ThrowAsync<UnauthorizedException>();
+    }
+
+    private string RandomToken(int length)
+    {
+        Random random = new Random();
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+}
